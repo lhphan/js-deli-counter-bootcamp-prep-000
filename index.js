3d713263e80962f48c87361acb876cac288c766a
Lien Phan <whyofcourse@gmail.com>
2017-09-12 22:25:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,13 @@ function takeANumber(katzDeli, name){
   katzDeli.push(name)
   return `Welcome, ${name}. You are number ${katzDeli.length} in line.`
 }
+
+function nowServing(katzDeli){
+  if(katzDeli >= 1){
+    var serving = katzDeli[0]
+    katzDeli.unshift()
+    return serving
+  }else{
+    return "There is nobody waiting to be served!"
+  }
+}
